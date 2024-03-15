6b7d7753e308bd6ac56b9344dd5ab3cb1bb2f449
pal-deepak <110769944+pal-deepak@users.noreply.github.com>
2024-03-15 23:11:25 +0530

    first commit

---
+++ src/component/About/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function About() {
+    return (
+        <div className="py-16 bg-white">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img className="rounded-full"
+                            src="https://cdn.gencraft.com/prod/user/f7de6e81-e551-4fcc-9954-468b05390169/fc907d98-670f-4f9b-a923-89b5b93c29ba/image/image0_0.jpg?Expires=1710601597&Signature=pJCptIMSjnORloxcvUKRmVpkexBdSS5KXwklLX8G21V2bGJJcsOOBUmpom3mK5QmQv5GLp7DoRmXwSYdGrLuRqyjQOK9s~EYZWyMicpmimNpNK0UOosJsxMfVPjdZJIjMK8~Y3C8-qoZyGUkyHhCcvhEYmDXeOq6hpCaUn1n4-VBul3vVyf1B8QF9jhok5as-29uOLmHBdVVN~9ON0p~fZgmY5JbzWeYIgCBtfFHO63XkmxWfc3FMfGfJdUwDU0QilxhWTJb7Yekbao4EokkCaqO69jnj7CTUP7xkFxLAOGrEKnKlvgLVL7Kb3sKyApbny-WhlTh16QGhUgOD8m~iw__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+                            alt="image"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                        Journalism Unveiled
+                        </h2>
+                        <h1 className="text-1xl font-semibold ">I am journalistic luminary, has spent decades on the frontline, unraveling stories that echo through the corridors of truth.
+                        A maestro in investigative reporting,  My pen has danced across pages, revealing narratives that transcend the ordinary
+                        With an unwavering commitment to journalistic integrity, I has carved a legacy, blending curiosity with a profound sense of responsibility.
+                        In the world of newsprint,I stands tall, his bylines a testament to a career dedicated to unearthing the stories that matter.
+                        As a seasoned journalist, I keen insight and storytelling prowess have painted vivid portraits of societal shifts and cultural nuances.
+                        Through the lens of I reporting, the ordinary becomes extraordinary, and the silence of untold stories finds its voice
+                        A wordsmith of the highest order, I narratives have the power to captivate minds, weaving a tapestry of facts and emotions
+                        In the ever-evolving landscape of journalism, I foresight and adaptability have made him a beacon, guiding the profession into new horizons
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
