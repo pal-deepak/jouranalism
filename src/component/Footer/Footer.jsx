38a45b33274956b71deba6c560de123abe6daa05
pal-deepak <110769944+pal-deepak@users.noreply.github.com>
2024-07-12 15:02:41 +0530

    Update Footer.jsx

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -64,7 +64,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                            deepak
                         </a>
                         . All Rights Reserved.
                     </span>
